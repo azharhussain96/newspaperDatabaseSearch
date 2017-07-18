b626a43bbee60d6cfd7b04e763e6b1cfd14d8376
Azhar Hussain <azhar.m.hussain.19@dartmouth.edu>
2017-07-18 19:46:07 -0400

    cleaned up script

--- newspaperSearch.py
+++ newspaperSearch.py
@@ -15,9 +15,11 @@ import requests
 
 def searchDatabase(startDate, endDate, searchText, stateArray):
 
+    # Database imposed year range
     STARTYEAR = 1789
     ENDYEAR = 1924
 
+    # check if all arguments are present
     if len(sys.argv) >= 5:
 
         if startDate >= STARTYEAR and endDate <= ENDYEAR:
@@ -29,16 +31,17 @@ def searchDatabase(startDate, endDate, searchText, stateArray):
 
                 for year in range(startDate, endDate + 1):
 
+                    # create url and return html from results page
                     url = "http://chroniclingamerica.loc.gov/search/pages/results/?state=" + state + "&date1=" + \
                           str(year) + "&date2=" + str(year) + "&proxtext=" + searchText \
                           + "&x=17&y=16&dateFilterType=yearRange&rows=20&searchType=basic"
                     page = requests.get(url)
 
-                    line = page.text
-                    matchObj = re.search('<p class="term">(.*) results', line)
+                    # search for results pattern
+                    matchObj = re.search('<p class="term">(.*) results', page.text)
 
+                    # if matches
                     if matchObj:
-                        year = str(year)
                         matches = matchObj.group(1) + " matches"
                         result = '{:>45}| {:>12}| {:>6}| {:>14}|'.format("",state, year, matches)
                         print(result)
@@ -52,5 +55,6 @@ def searchDatabase(startDate, endDate, searchText, stateArray):
     else:
         sys.stderr.write("usage: newspaperSearch.py startDate endDate searchWord state...")
 
+# state argumentes provided past index 4
 stateArray = sys.argv[4:]
 searchDatabase(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3], stateArray)
